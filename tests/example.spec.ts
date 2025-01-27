b80fda52f67e1f4730ae248bc66e38401f11a29e
Daneric <danericaalbers@hotmail.com>
2025-01-27 14:03:24 +0100

    WIP: testing playwright setup

---
+++ tests/example.spec.ts
@@ -0,0 +1,20 @@
+import { test, expect } from "@playwright/test";
+
+test.beforeEach(({ page }) => page.goto("https://playwright.dev/"));
+
+test("should succeed", async ({ page }) => {
+  await expect(page).toHaveTitle(/Playwright/);
+});
+
+test("should fail", async ({ page }) => {
+  await expect(page).not.toHaveTitle(/Playwright/);
+});
+
+test("should be flaky", async ({ page }) => {
+  if (Math.random() > 0.5) {
+    console.log(Math.random());
+    await expect(page).toHaveTitle(/Playwright/);
+  } else {
+    await expect(page).not.toHaveTitle(/Playwright/);
+  }
+});
